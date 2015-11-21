8339eab35feff2fd24cffed48e38f35e60f27296
Xavier Tristancho <xavi@letitguide.com>
2015-11-21 13:04:58 +0100

    First Commit

---
+++ office/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  office
+//
+//  Created by Xavier Tristancho Bordoy on 21/11/15.
+//  Copyright © 2015 Xavier Tristancho Bordoy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ officeTests/officeTests.m
@@ -0,0 +1,39 @@
+//
+//  officeTests.m
+//  officeTests
+//
+//  Created by Xavier Tristancho Bordoy on 21/11/15.
+//  Copyright © 2015 Xavier Tristancho Bordoy. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface officeTests : XCTestCase
+
+@end
+
+@implementation officeTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
